332ca0f2db1189383f34d8e0dbaf645b1afa42c8
venkatesh-r <venkypsd@gmail.com>
2024-12-29 06:20:09 +0100

    cors added

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,18 @@
 import express, { Application } from "express";
+import cors from "cors";
 const bodyParser = require("body-parser");
 import connectDB from "./config/database";
 import productRouter from "./routes/productRoutes";
 
 const app: Application = express();
+
+//CORS
+app.use(cors());
+
+const corsOptions = {
+  origin: "http://localhost:5000/",
+  optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
+};
 
 //Middleware parse JSON
 app.use(bodyParser.json());
